b84f072601e9f8a608821da5bcffb388dc73058e
Johnathan Reese <JohnathanReese8@gmail.com>
2022-04-24 22:34:35 -0400

    Redid everything based on photo-port. Need to fix links to Resume and Portfolio pages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import ContactForm from './components/Contact';
 
-const App = () => <PortfolioContainer />;
+function App() {
+  const [categories] = useState([
+    { name: 'portfolio'},
+    { name: 'resume'},
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import cctImage from '../../assets/images/cct.png';
+import soeImage from '../../assets/images/soe.png';
+function Portfolio() {
+  return (
+    <section className="my-5">
+      <img src={cctImage} className="my-2" style={{ width: "100%" }} alt="Cocky Cocktails" />
+      <img src={soeImage} className="my-2" style={{ width: "100%" }} alt="Stack Overeats" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
+          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
+          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
+          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
+          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Portfolio;
