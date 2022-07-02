af559c6792584763a5f8f21de4e9707259cae588
Johnathan Reese <JohnathanReese8@gmail.com>
2022-07-02 19:51:06 -0400

    Updates

---
+++ precache-manifest.99ff0246f129573efa4ad6f74c7c1dab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "070ff25963bc26e86cc2",
+    "url": "/johnathan-react-portfolio/static/css/main.e04d1e13.chunk.css"
+  },
+  {
+    "revision": "070ff25963bc26e86cc2",
+    "url": "/johnathan-react-portfolio/static/js/main.070ff259.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/johnathan-react-portfolio/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "b58f531df19d320e9415",
+    "url": "/johnathan-react-portfolio/static/css/2.3923e3a3.chunk.css"
+  },
+  {
+    "revision": "b58f531df19d320e9415",
+    "url": "/johnathan-react-portfolio/static/js/2.b58f531d.chunk.js"
+  },
+  {
+    "revision": "dc4f94a78c77104ee5cb",
+    "url": "/johnathan-react-portfolio/static/js/runtime~main.dc4f94a7.js"
+  },
+  {
+    "revision": "eee4df7314feb2b65e5e39699f379741",
+    "url": "/johnathan-react-portfolio/static/media/cct.eee4df73.png"
+  },
+  {
+    "revision": "b9cf171ae21bb198075c8ca2b320df9c",
+    "url": "/johnathan-react-portfolio/static/media/soe.b9cf171a.png"
+  },
+  {
+    "revision": "1c9854d1504749238f0306981ef5197a",
+    "url": "/johnathan-react-portfolio/static/media/hssc.1c9854d1.png"
+  },
+  {
+    "revision": "a371f11290d3e6bf24e2c0df68cf902e",
+    "url": "/johnathan-react-portfolio/static/media/me.a371f112.JPG"
+  },
+  {
+    "revision": "d80e6b1c2a0cecf8902fde3513b01687",
+    "url": "/johnathan-react-portfolio/static/media/hero-bg.d80e6b1c.jpg"
+  },
+  {
+    "revision": "85399ed70b07989cfc49be79c0efdbc2",
+    "url": "/johnathan-react-portfolio/index.html"
+  }
+];
